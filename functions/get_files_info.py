fb2574322796c4745cbc033c4f51a2609e5036b1
boorivard <boorivard@comcast.net>
2025-12-16 14:43:24 -0500

    comments

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,6 +2,7 @@ import os
 
 def get_files_info(working_directory, directory="."):
     try:
+        #get working path, target directory, and check to see if target directory is valid
         working_dir_abs = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
@@ -12,11 +13,14 @@ def get_files_info(working_directory, directory="."):
         if not os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
         
+        #create list of content in target directory
         target_dir_contents = os.listdir(target_dir)
         files_info = []
+        #iterate through each item and add a string of the name, size, and if it is a directory to a list
         for filename in target_dir_contents:
             filepath = os.path.join(target_dir, filename)
             files_info.append(f"- {filename}: file_size={os.path.getsize(filepath)}, is_dir={os.path.isdir(filepath)}")
+        #return a joined list of all the items in the directory with newline as the delimiter
         return "\n".join(files_info)
     except Exception as e:
         return f"Error: {e}"

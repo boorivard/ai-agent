890ef593de298351d5d0afd6cba7643927e6cba1
boorivard <boorivard@comcast.net>
2025-12-16 13:41:24 -0500

    adding verbose flag +  future conversation capabilities

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 load_dotenv()
@@ -13,17 +14,21 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("prompt", type=str, help="Prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
+messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
     
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=args.prompt
+    model='gemini-2.5-flash', contents=messages
 )
 
 if(response.usage_metadata is None):
     raise RuntimeError("metadata for this prompt could not be found")
 
-print(f"Prompt: {args.prompt}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if(args.verbose == True):
+    print(f"User prompt: {args.prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(f"Response:\n {response.text}")
 

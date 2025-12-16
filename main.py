a45f096015896e5e5eb6c87fce2ba62c127ba96e
boorivard <boorivard@comcast.net>
2025-12-16 13:46:54 -0500

    adding calcualtor.py, tests.py, main.py for calc, render.py from boot.dev

--- main.py
+++ main.py
@@ -4,31 +4,34 @@ from google import genai
 from google.genai import types
 import argparse
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-if api_key is None:
-    raise RuntimeError("api key could not be found")
+    if api_key is None:
+        raise RuntimeError("api key could not be found")
 
-client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("prompt", type=str, help="Prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("prompt", type=str, help="Prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
-    
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=messages
-)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
+        
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=messages
+    )
 
-if(response.usage_metadata is None):
-    raise RuntimeError("metadata for this prompt could not be found")
+    if(response.usage_metadata is None):
+        raise RuntimeError("metadata for this prompt could not be found")
 
-if(args.verbose == True):
-    print(f"User prompt: {args.prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-print(f"Response:\n {response.text}")
+    if(args.verbose == True):
+        print(f"User prompt: {args.prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n {response.text}")
 
+if __name__ == "__main__":
+    main()

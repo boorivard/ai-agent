e8bef4509ff4a3fc55d83b60d648dec632765360
boorivard <boorivard@comcast.net>
2025-12-16 13:01:31 -0500

    tracking token usage

--- main.py
+++ main.py
@@ -14,5 +14,11 @@ response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
 
-print(response.text)
+if(response.usage_metadata is None):
+    raise RuntimeError("metadata for this prompt could not be found")
+
+print(f"Prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response:\n {response.text}")
 

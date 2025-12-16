8df21b15a9256ccf75e82dc65155f249898a8bd9
boorivard <boorivard@comcast.net>
2025-12-16 13:23:31 -0500

    proper prompting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,15 +10,19 @@ if api_key is None:
     raise RuntimeError("api key could not be found")
 
 client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("prompt", type=str, help="Prompt")
+args = parser.parse_args()
     
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model='gemini-2.5-flash', contents=args.prompt
 )
 
 if(response.usage_metadata is None):
     raise RuntimeError("metadata for this prompt could not be found")
 
-print(f"Prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(f"Prompt: {args.prompt}")
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 print(f"Response:\n {response.text}")

8b0a86840f825771c6b6036f1c4e5aa670130848
boorivard <boorivard@comcast.net>
2025-12-16 14:40:07 -0500

    adding function to get files for agent

---
+++ functions/get_files_info.py
@@ -0,0 +1,22 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        
+        target_dir_contents = os.listdir(target_dir)
+        files_info = []
+        for filename in target_dir_contents:
+            filepath = os.path.join(target_dir, filename)
+            files_info.append(f"- {filename}: file_size={os.path.getsize(filepath)}, is_dir={os.path.isdir(filepath)}")
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error: {e}"

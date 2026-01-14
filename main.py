84e70468fe056dd9993e74e8f07964cfe871fa24
boorivard <boorivard@comcast.net>
2026-01-14 18:34:40 -0500

    added the ability for the agent to actually call and utilize the functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main():
     #load in API key
@@ -42,12 +42,25 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+    function_responses =[]
+
     #print function calls or a response
     if response.function_calls:
         for call in response.function_calls:
-            print(f"Calling function: {call.name}({call.args})\n")
+            function_call_result = call_function(call)
+            if function_call_result.parts is None:
+                raise Exception("parts metatdata is empty")
+            if function_call_result.parts[0].function_response is None:
+                raise Exception("function reponse is not a FunctionResponse object")
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception("response of result is None")
+            function_responses.append(function_call_result.parts[0])
+            if(args.verbose==True):
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print(f"Response:\n {response.text}")
+
+    
 
 if __name__ == "__main__":
     main()
